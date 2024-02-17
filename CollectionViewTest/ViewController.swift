874aa037d1e3b96ca1cf8b44bec6813987a0829d
Marcello <morellato.m@gmail.com>
2024-02-17 17:28:36 +0100

    add leadingSwipeActionsConfigurationProvider

--- CollectionViewTest/ViewController.swift
+++ CollectionViewTest/ViewController.swift
@@ -6,11 +6,10 @@
 //
 
 import UIKit
-
-import UIKit
+import Foundation
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
+    let cellHeight = 290.0
     @IBOutlet weak var collectionView: UICollectionView! // Connect this to your UICollectionView in the storyboard
     
     override func viewDidLoad() {
@@ -19,7 +18,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         // Set the collectionView's dataSource and delegate
         collectionView.dataSource = self
         collectionView.delegate = self
-        
+        collectionView.collectionViewLayout = createLayout()
         // Register the custom cell
         collectionView.register(UINib(nibName: "TestCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "TestCollectionViewCell")
     }
@@ -49,3 +48,73 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 }
 
 
+extension ViewController {
+    private func createLayout() -> UICollectionViewLayout {
+        var listConfig = UICollectionLayoutListConfiguration(appearance: .plain)
+        
+        //TODO: weak self
+        listConfig.leadingSwipeActionsConfigurationProvider = { indexPath in
+            let item = self.collectionView.cellForItem(at: indexPath)
+            
+           
+            let actionHandler1: UIContextualAction.Handler = { action, view, completion in
+              completion(true)
+                self.collectionView.reloadItems(at: [indexPath])
+            }
+            let action1 = UIContextualAction(style: .normal, title: "L", handler: actionHandler1)
+            action1.backgroundColor = BasketOrderLineType.normal.colorRepresentation()
+
+            let actionHandler2: UIContextualAction.Handler = { action, view, completion in
+              completion(true)
+                self.collectionView.reloadItems(at: [indexPath])
+            }
+            let action2 = UIContextualAction(style: .normal, title: "PZ", handler: actionHandler2)
+            action2.backgroundColor = BasketOrderLineType.specialPrice.colorRepresentation()
+            
+            let actionHandler3: UIContextualAction.Handler = { action, view, completion in
+              completion(true)
+                self.collectionView.reloadItems(at: [indexPath])
+            }
+            let action3 = UIContextualAction(style: .normal, title: "S", handler: actionHandler3)
+            action3.backgroundColor = BasketOrderLineType.discountNature.colorRepresentation()
+
+            return UISwipeActionsConfiguration(actions: [action1, action2, action3])
+        }
+        
+        listConfig.showsSeparators = false
+        
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfig)
+        
+        return listLayout
+    }
+    
+    func configSize(){
+        let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(1024.0),
+                                              heightDimension: .absolute(cellHeight))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalWidth(1.0))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+    }
+}
+
+enum BasketOrderLineType: Int {
+    case normal = 0, specialPrice = 1, discountNature = 2
+        
+    func colorRepresentation() -> UIColor {
+        switch self {
+        case .specialPrice:
+            return UIColor.orange
+        case .discountNature:
+            return UIColor.red
+        default:
+            return UIColor.blue
+        }
+    }
+}
