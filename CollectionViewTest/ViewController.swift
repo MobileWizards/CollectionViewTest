ac3dae5395e392b7e12b1195870bb45245d65895
Marcello <morellato.m@gmail.com>
2024-02-17 17:03:14 +0100

    Initial Commit

---
+++ CollectionViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by Marcello Morellato on 17/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
